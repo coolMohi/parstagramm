c8a6154c18e244813d73e8f6fbc9c2c9d5910bcd
coolMohi <coolmohhi@gmail.com>
2022-10-06 17:52:00 -0500

    Initial Commit

---
+++ parstagramm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  parstagramm
+//
+//  Created by Ahmed Mohammed on 10/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
